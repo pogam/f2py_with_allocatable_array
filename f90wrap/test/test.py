c2c932a6ad2e1e6e44d12727c42f17b23c57d53a
ronan <ronan.paugam@gmail.com>
2020-04-12 12:33:26 +0200

    COREFF: test in python3

--- f90wrap/test/test.py
+++ f90wrap/test/test.py
@@ -5,9 +5,9 @@ import mymodule
 
 a = 1
 mymodule.module_structure.init()
-print '##from pyhton: call do_something_else (+=1)'
+print('##from pyhton: call do_something_else (+=1)')
 b = mymodule.module_structure.do_something_else(a)
-print '##from pyhton,result', b, '(should be = 2)'
+print('##from pyhton,result', b, '(should be = 2)')
 mymodule.module_structure.print_on_screen()
-print '##from pyhton, this should count to 20'
+print('##from pyhton, this should count to 20')
 
